288976ea9b93a41c5160f93ffc44ae34f10b2575
gangstar <finaly.catch.try@gmail.com>
2022-05-09 20:02:08 +0300

    add sign-up modal

---
+++ js/form-validation.js
@@ -0,0 +1,15 @@
+document.querySelector('#sign-up-form.needs-validation').addEventListener('submit',
+    function (event) {
+        const [pwInput, pw2Input] = document.querySelectorAll("#sign-up-form input[type='password']");
+        const pwMatches = pwInput.value === pw2Input.value;
+
+        if (this.checkValidity() && pwMatches)
+            return;
+
+        if (!pwMatches)
+            pw2Input.nextElementSibling.classList.add("d-block");
+
+        event.stopPropagation();
+        event.preventDefault();
+        this.classList.add("was-validated")
+    }, false);
